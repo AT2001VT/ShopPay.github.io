62b5246c420e7ae48fb7a7b4086485653f67f427
Atreya <atreya.tiwari2001@gmail.com>
2021-12-10 19:34:47 +0530

    E-commerce website

---
+++ src/containers/Header.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+
+
+const Header= ()=>{
+
+    return (
+        <div className="ui fied menu">
+            <div className="ui container center">
+               
+                <h1></h1>
+                        
+                
+                <div class="nine">
+  <h1><em>ShopPay</em></h1>
+</div>
+
+
+                <h3> One stop destination for all your wishes!</h3>
+                <button class="button-15" role="button">Search</button>
+                <button class="button-15" role="button">My Orders</button>
+                <button class="button-15" role="button">My Wishlist</button>
+                <button class="button-15" role="button">Cart</button>
+                <button class="button-62" role="button">My Account</button>
+            </div>
+        </div>
+    )
+
+}
+
+
+
+export default Header;
